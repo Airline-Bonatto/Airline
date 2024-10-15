8871d3f318219ef47b07b0adc7e41166af865eba
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2024-10-15 07:20:32 -0300

    Add models

---
+++ Models/Aircraft.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineAPIV2.Models
+{
+    public class Aircraft
+    {
+        public int AircraftID {  get; set; }
+        public virtual ICollection<Route>? Routes { get; set; } = [];
+        public required string Model { get; set; }
+        public int Capacity { get; set; } 
+        public double Range { get; set; }  
+
+    }
+}
---
+++ Models/Route.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineAPIV2.Models
+{
+    public class Route
+    {
+        public int RouteID { get; set; }
+
+        public required virtual Aircraft Aircraft { get; set; }
+        public required string From { get; set; }
+        public required string To { get; set; }
+        public double Distance { get; set; }
+    }
+}
