160b8b0850525c10c134ddb3f03b41eadd6119da
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2024-10-15 07:20:14 -0300

    Add database config

---
+++ Database/Database/AirlineContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+using AirlineAPIV2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Airline.Database
+{
+    public class AirlineContext : DbContext
+    {
+
+        public DbSet<Aircraft> Aircraft {  get; set; }
+        public DbSet<AirlineAPIV2.Models.Route> Route { get; set; }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Airline;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+        }
+
+    }
+}
---
+++ Database/Database/DAL.cs
@@ -0,0 +1,75 @@
+﻿using Airline.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline.DAL
+{
+    public class DAL<T> where T : class
+    {
+        private readonly AirlineContext context;
+
+        public DAL(AirlineContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<T> List()
+        {
+            try
+            {
+                return context.Set<T>().ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Aircrafts list error!");
+                Console.WriteLine(e.Message);
+                return [];
+            }
+        }
+        public void Register(T model)
+        {
+            try
+            {
+                context.Set<T>().Add(model);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Model register error!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        public void Update(T model)
+        {
+            try
+            {
+                context.Set<T>().Update(model);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Aircraft update error!");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Remove(T model)
+        {
+            try
+            {
+                context.Set<T>().Remove(model);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Aircraft remove error!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
