4e0c14564a5284dc067b535e4f42b1e6d7c08ac5
Gabriel Bonatto Justo <gjusto@nelogica.com.br>
2025-10-08 11:45:24 -0300

    Add seat create tests.

---
+++ Airline.Tests/SampleTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Airline.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
